cf05b004bd1cda4c050f5fbbe2ff48c80b6cfaaa
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2017-02-04 17:45:01 -0600

    Add live reloading in gulp Populate main view

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', () => {
+gulp.task('build', () => {
   gulp.src('./sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/styles'));
@@ -14,3 +15,23 @@ gulp.task('default', () => {
   gulp.src('./views/index.html')    
     .pipe(gulp.dest('./build/'));
 });
+
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+        server: "./build",
+        open: false,
+        notify: false
+    });
+
+    gulp.watch("./sass/*.scss", ['sass']);
+    gulp.watch("./views/*.html").on('change', browserSync.reload);
+});
+
