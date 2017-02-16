9e0b43311f2a406f849cad4b577cb8ef89d1f8e1
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2017-02-15 18:12:23 -0600

    Fix layout issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,22 @@
 'use strict';
-
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const ghPages = require('gulp-gh-pages');
 
 
 gulp.task('build', () => {
+gulp.src('./images/**.*')
+	.pipe(gulp.dest('./build/images'));
 
-  gulp.src('./images/**.*')
-    .pipe(gulp.dest('./build/images'));
+gulp.src('./index.html')
+	.pipe(gulp.dest('./build/'));
 
- 	 gulp.src('./index.html')
-    .pipe(gulp.dest('./build/'));
+gulp.src('./fragments/**.html')
+	.pipe(gulp.dest('./build/fragments'));
 
-	gulp.src('./fragments/**.html')
-    .pipe(gulp.dest('./build/fragments'));
-
-	gulp.src('./data/**.json')
-    .pipe(gulp.dest('./build/data'));
+gulp.src('./data/**.json')
+	.pipe(gulp.dest('./build/data'));
 
 
-	gulp.src('./bower_components/**/**')
+gulp.src('./bower_components/**/**')
 	.pipe(gulp.dest('./build/bower_components'));
 
 });
-
-
-gulp.task('deploy', () => {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
-});
-
-let buildAndRefresh = function() {
-	gulp.start('build');
-	browserSync.reload();
-}
-
-gulp.task('serve', () => {
-  browserSync.init({
-        server: "./build",
-        open: false,
-        notify: false
-    });
-
-	gulp.watch("./fragments/**.html").on('change', buildAndRefresh)
-	gulp.watch("./index.html").on('change', buildAndRefresh);
-
-});
-
